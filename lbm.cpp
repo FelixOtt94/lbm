af3de5aaf946fdf4ddb5dfda00b1fe9d499c729e
Juliankarl33 <bu43jazu@stud.informatik.uni-erlangen.de>
2016-06-28 10:14:46 +0200

    first commit

---
+++ lbm.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <stdlib.h>
+#include <string>
+#include <stdio.h>
+
+#include "imageClass/GrayScaleImage.h"
+#include "imageClass/lodepng.h"
+
+
+
+
+int main( int args, char** argv ){
+
+
+
+    exit( EXIT_SUCCESS );
+}
